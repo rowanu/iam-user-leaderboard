e6748dab3d600ada5f84172d204ea94619c76ce5
rowan <rowan@rowanudell.com>
2024-03-05 19:31:11 +1000

    feat: hide raw data by default

--- src/components/ReadCSV.vue
+++ src/components/ReadCSV.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="space-y-4">
     <div>
-      Generate an load an <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_getting-report.html"
-        target="_blank">IAM credential
-        report</a>.
+      Load an <a href="https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_getting-report.html"
+        target="_blank">IAM credential report</a>.
       Your credential report file does not contain any credentials, and is not shared externally.
       All processing happens locally in your browser.
     </div>
-    <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+    <div class="mt-1 flex px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
       <div class="space-y-1 text-center">
         <div class="flex text-sm text-gray-600">
           <button type="button"
             class="group relative w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm text-left text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             @click="$refs.fileInput.click()">
             <input type="file" ref="fileInput" class="hidden" accept=".csv" @change="uploadFile($event)">
-            <img src="../assets/csv.svg" alt="A CSV file" class="w-6 h-6 inline-block mr-2 group-hover:text-indigo-600" />
+            <img src="../assets/csv.svg" alt="A CSV file"
+              class="w-6 h-6 inline-block mr-2 group-hover:text-indigo-600" />
             <span>Load a file</span>
           </button>
         </div>
+      </div>
+    </div>
+    <div>
+      <h3 class="text-xl">Key</h3>
+      <div>
+        <FontAwesomeIcon icon="fingerprint" /> MFA shoould enabled if the user uses a password, or is a root user.
+        <FontAwesomeIcon icon="key" /> Access keys should only be used by users with no password.
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { parse } from 'papaparse'
 
 const emit = defineEmits(['parsed-data'])
--- src/components/UserBoard.vue
+++ src/components/UserBoard.vue
@@ -1,78 +1,79 @@
 <template>
-  <table class="w-full text-center">
-    <tr>
-      <th>User</th>
-      <th>ARN</th>
-      <th>MFA enabled?</th>
-    </tr>
-    <tr v-for="item in enrichedData" :key="item.user">
-      <td>{{ item.user }}</td>
-      <td>{{ item.arn }}</td>
-      <td>
+  <div class="w-full p-4 m-4">
+    <ol class="list-decimal">
+      <li v-for="item in enrichedData" :key="item.user" class="p-1">
+        {{ item.user }}
         <span v-if="item.mfa_fail">
           <FontAwesomeIcon icon="fingerprint" class="text-red-500" /> FAIL
         </span>
         <span v-else>
           <FontAwesomeIcon icon="fingerprint" class="text-green-500" /> PASS
         </span>
-      </td>
-    </tr>
-  </table>
-  <table class="w-full">
-    <tr>
-      <th>User</th>
-      <th>ARN</th>
-      <th>User Creation Time</th>
-      <th>Password Enabled</th>
-      <th>Password Last Used</th>
-      <th>Password Last Changed</th>
-      <th>Password Next Rotation</th>
-      <th>MFA Active</th>
-      <th>Access Key 1 Active</th>
-      <th>Access Key 1 Last Rotated</th>
-      <th>Access Key 1 Last Used Date</th>
-      <th>Access Key 1 Last Used Region</th>
-      <th>Access Key 1 Last Used Service</th>
-      <th>Access Key 2 Active</th>
-      <th>Access Key 2 Last Rotated</th>
-      <th>Access Key 2 Last Used Date</th>
-      <th>Access Key 2 Last Used Region</th>
-      <th>Access Key 2 Last Used Service</th>
-      <th>Cert 1 Active</th>
-      <th>Cert 1 Last Rotated</th>
-      <th>Cert 2 Active</th>
-      <th>Cert 2 Last Rotated</th>
-    </tr>
-    <tr v-for="item in enrichedData" :key="item.user">
-      <td>{{ item.user }}</td>
-      <td>{{ item.arn }}</td>
-      <td>{{ item.user_creation_time }}</td>
-      <td>{{ item.password_enabled }}</td>
-      <td>{{ item.password_last_used }}</td>
-      <td>{{ item.password_last_changed }}</td>
-      <td>{{ item.password_next_rotation }}</td>
-      <td>{{ item.mfa_active }}</td>
-      <td>{{ item.access_key_1_active }}</td>
-      <td>{{ item.access_key_1_last_rotated }}</td>
-      <td>{{ item.access_key_1_last_used_date }}</td>
-      <td>{{ item.access_key_1_last_used_region }}</td>
-      <td>{{ item.access_key_1_last_used_service }}</td>
-      <td>{{ item.access_key_2_active }}</td>
-      <td>{{ item.access_key_2_last_rotated }}</td>
-      <td>{{ item.access_key_2_last_used_date }}</td>
-      <td>{{ item.access_key_2_last_used_region }}</td>
-      <td>{{ item.access_key_2_last_used_service }}</td>
-      <td>{{ item.cert_1_active }}</td>
-      <td>{{ item.cert_1_last_rotated }}</td>
-      <td>{{ item.cert_2_active }}</td>
-      <td>{{ item.cert_2_last_rotated }}</td>
-    </tr>
-  </table>
+      </li>
+    </ol>
+  </div>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    @click="showRawData = !showRawData">Show Raw Data</button>
+  <div :class="{ hidden: !showRawData }">
+    <h3 class="text-xl">Raw data</h3>
+    <table class="w-full">
+      <tr>
+        <th>User</th>
+        <th>ARN</th>
+        <th>User Creation Time</th>
+        <th>Password Enabled</th>
+        <th>Password Last Used</th>
+        <th>Password Last Changed</th>
+        <th>Password Next Rotation</th>
+        <th>MFA Active</th>
+        <th>Access Key 1 Active</th>
+        <th>Access Key 1 Last Rotated</th>
+        <th>Access Key 1 Last Used Date</th>
+        <th>Access Key 1 Last Used Region</th>
+        <th>Access Key 1 Last Used Service</th>
+        <th>Access Key 2 Active</th>
+        <th>Access Key 2 Last Rotated</th>
+        <th>Access Key 2 Last Used Date</th>
+        <th>Access Key 2 Last Used Region</th>
+        <th>Access Key 2 Last Used Service</th>
+        <th>Cert 1 Active</th>
+        <th>Cert 1 Last Rotated</th>
+        <th>Cert 2 Active</th>
+        <th>Cert 2 Last Rotated</th>
+      </tr>
+      <tr v-for="item in enrichedData" :key="item.user">
+        <td>{{ item.user }}</td>
+        <td>{{ item.arn }}</td>
+        <td>{{ item.user_creation_time }}</td>
+        <td>{{ item.password_enabled }}</td>
+        <td>{{ item.password_last_used }}</td>
+        <td>{{ item.password_last_changed }}</td>
+        <td>{{ item.password_next_rotation }}</td>
+        <td>{{ item.mfa_active }}</td>
+        <td>{{ item.access_key_1_active }}</td>
+        <td>{{ item.access_key_1_last_rotated }}</td>
+        <td>{{ item.access_key_1_last_used_date }}</td>
+        <td>{{ item.access_key_1_last_used_region }}</td>
+        <td>{{ item.access_key_1_last_used_service }}</td>
+        <td>{{ item.access_key_2_active }}</td>
+        <td>{{ item.access_key_2_last_rotated }}</td>
+        <td>{{ item.access_key_2_last_used_date }}</td>
+        <td>{{ item.access_key_2_last_used_region }}</td>
+        <td>{{ item.access_key_2_last_used_service }}</td>
+        <td>{{ item.cert_1_active }}</td>
+        <td>{{ item.cert_1_last_rotated }}</td>
+        <td>{{ item.cert_2_active }}</td>
+        <td>{{ item.cert_2_last_rotated }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
+
+let showRawData = ref(false)
 
 const props = defineProps({
   data: {
